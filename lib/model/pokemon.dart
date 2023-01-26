16400d332cdfe01aa362fcaefb53818194f460d7
Ryota <ryoenoki2013@gmail.com>
2023-01-26 19:04:59 +0900

    update: models

--- lib/model/pokemon.dart
+++ lib/model/pokemon.dart
@@ -1,4 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
 import 'package:pokemon_catcher/model/sprites.dart';
 
 class Pokemon {
@@ -17,4 +19,76 @@ class Pokemon {
   final int height;
   final int weight;
   final Sprites sprites;
+
+  Pokemon copyWith({
+    int? id,
+    String? name,
+    int? order,
+    int? height,
+    int? weight,
+    Sprites? sprites,
+  }) {
+    return Pokemon(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      order: order ?? this.order,
+      height: height ?? this.height,
+      weight: weight ?? this.weight,
+      sprites: sprites ?? this.sprites,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'name': name,
+      'order': order,
+      'height': height,
+      'weight': weight,
+      'sprites': sprites.toMap(),
+    };
+  }
+
+  factory Pokemon.fromMap(Map<String, dynamic> map) {
+    return Pokemon(
+      id: map['id'] as int,
+      name: map['name'] as String,
+      order: map['order'] as int,
+      height: map['height'] as int,
+      weight: map['weight'] as int,
+      sprites: Sprites.fromMap(map['sprites'] as Map<String, dynamic>),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Pokemon.fromJson(String source) =>
+      Pokemon.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'Pokemon(id: $id, name: $name, order: $order, height: $height, weight: $weight, sprites: $sprites)';
+  }
+
+  @override
+  bool operator ==(covariant Pokemon other) {
+    if (identical(this, other)) return true;
+
+    return other.id == id &&
+        other.name == name &&
+        other.order == order &&
+        other.height == height &&
+        other.weight == weight &&
+        other.sprites == sprites;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+        name.hashCode ^
+        order.hashCode ^
+        height.hashCode ^
+        weight.hashCode ^
+        sprites.hashCode;
+  }
 }
--- lib/model/sprites.dart
+++ lib/model/sprites.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
 class Sprites {
   Sprites({
     required this.frontDefault,
@@ -12,6 +15,72 @@ class Sprites {
   final String backDefault;
   final String backShiny;
   final Other other;
+
+  Sprites copyWith({
+    String? frontDefault,
+    String? frontShiny,
+    String? backDefault,
+    String? backShiny,
+    Other? other,
+  }) {
+    return Sprites(
+      frontDefault: frontDefault ?? this.frontDefault,
+      frontShiny: frontShiny ?? this.frontShiny,
+      backDefault: backDefault ?? this.backDefault,
+      backShiny: backShiny ?? this.backShiny,
+      other: other ?? this.other,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'frontDefault': frontDefault,
+      'frontShiny': frontShiny,
+      'backDefault': backDefault,
+      'backShiny': backShiny,
+      'other': other.toMap(),
+    };
+  }
+
+  factory Sprites.fromMap(Map<String, dynamic> map) {
+    return Sprites(
+      frontDefault: map['frontDefault'] as String,
+      frontShiny: map['frontShiny'] as String,
+      backDefault: map['backDefault'] as String,
+      backShiny: map['backShiny'] as String,
+      other: Other.fromMap(map['other'] as Map<String, dynamic>),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Sprites.fromJson(String source) =>
+      Sprites.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'Sprites(frontDefault: $frontDefault, frontShiny: $frontShiny, backDefault: $backDefault, backShiny: $backShiny, other: $other)';
+  }
+
+  @override
+  bool operator ==(covariant Sprites o) {
+    if (identical(this, o)) return true;
+
+    return o.frontDefault == frontDefault &&
+        o.frontShiny == frontShiny &&
+        o.backDefault == backDefault &&
+        o.backShiny == backShiny &&
+        o.other == other;
+  }
+
+  @override
+  int get hashCode {
+    return frontDefault.hashCode ^
+        frontShiny.hashCode ^
+        backDefault.hashCode ^
+        backShiny.hashCode ^
+        other.hashCode;
+  }
 }
 
 class Other {
@@ -20,6 +89,45 @@ class Other {
   });
 
   final OfficialArtwork officialArtwork;
+
+  Other copyWith({
+    OfficialArtwork? officialArtwork,
+  }) {
+    return Other(
+      officialArtwork: officialArtwork ?? this.officialArtwork,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'officialArtwork': officialArtwork.toMap(),
+    };
+  }
+
+  factory Other.fromMap(Map<String, dynamic> map) {
+    return Other(
+      officialArtwork: OfficialArtwork.fromMap(
+          map['officialArtwork'] as Map<String, dynamic>),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Other.fromJson(String source) =>
+      Other.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() => 'Other(officialArtwork: $officialArtwork)';
+
+  @override
+  bool operator ==(covariant Other other) {
+    if (identical(this, other)) return true;
+
+    return other.officialArtwork == officialArtwork;
+  }
+
+  @override
+  int get hashCode => officialArtwork.hashCode;
 }
 
 class OfficialArtwork {
@@ -30,4 +138,47 @@ class OfficialArtwork {
 
   final String frontDefault;
   final String frontShiny;
+
+  OfficialArtwork copyWith({
+    String? frontDefault,
+    String? frontShiny,
+  }) {
+    return OfficialArtwork(
+      frontDefault: frontDefault ?? this.frontDefault,
+      frontShiny: frontShiny ?? this.frontShiny,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'frontDefault': frontDefault,
+      'frontShiny': frontShiny,
+    };
+  }
+
+  factory OfficialArtwork.fromMap(Map<String, dynamic> map) {
+    return OfficialArtwork(
+      frontDefault: map['frontDefault'] as String,
+      frontShiny: map['frontShiny'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory OfficialArtwork.fromJson(String source) =>
+      OfficialArtwork.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() =>
+      'OfficialArtwork(frontDefault: $frontDefault, frontShiny: $frontShiny)';
+
+  @override
+  bool operator ==(covariant OfficialArtwork other) {
+    if (identical(this, other)) return true;
+
+    return other.frontDefault == frontDefault && other.frontShiny == frontShiny;
+  }
+
+  @override
+  int get hashCode => frontDefault.hashCode ^ frontShiny.hashCode;
 }
