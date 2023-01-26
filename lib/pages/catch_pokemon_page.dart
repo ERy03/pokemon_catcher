94c51f8b7b1d2573b8792862f3da4495e596878e
Ryota <ryoenoki2013@gmail.com>
2023-01-26 18:23:18 +0900

    add: pokeball image and animation

--- lib/pages/catch_pokemon_page.dart
+++ lib/pages/catch_pokemon_page.dart
@@ -7,16 +7,69 @@ class CatchPokemonPage extends StatefulWidget {
   State<CatchPokemonPage> createState() => _CatchPokemonPageState();
 }
 
-class _CatchPokemonPageState extends State<CatchPokemonPage> {
+class _CatchPokemonPageState extends State<CatchPokemonPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> animation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+    animation =
+        CurvedAnimation(parent: controller, curve: Curves.easeInOutCirc);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Catch Pokemon'),
       ),
-      body: const SafeArea(
+      body: SafeArea(
         child: Center(
-          child: Text('Catch Pokemon'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              RotationTransition(
+                turns: animation,
+                child: SizedBox(
+                  height: 90,
+                  width: 90,
+                  child: Image.asset('assets/images/pokeball.png'),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  controller.repeat();
+                },
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xfff00000),
+                    foregroundColor: const Color(0xffee1515),
+                    surfaceTintColor: Colors.white,
+                    elevation: 3,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    )),
+                child: const Text(
+                  'Catch Pokemon',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
