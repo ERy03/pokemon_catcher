5f1e8d1bf37397f9079081f48313a5d225f4614e
Ryota <ryoenoki2013@gmail.com>
2023-01-27 11:57:55 +0900

    add: pokemon repository

---
+++ lib/repositories/pokemon_repository.dart
@@ -0,0 +1,26 @@
+import 'dart:math';
+
+import 'package:fpdart/fpdart.dart';
+import 'package:pokemon_catcher/model/pokemon.dart';
+import 'package:pokemon_catcher/services/rest_api_service.dart';
+
+abstract class PokemonRepository {
+  Future<Either<Exception, Pokemon>> getPokemon();
+}
+
+class PokemonRepositoryImpl implements PokemonRepository {
+  const PokemonRepositoryImpl(this._restApiService);
+
+  final RestApiServiceAbstract _restApiService;
+
+  @override
+  Future<Either<Exception, Pokemon>> getPokemon() async {
+    try {
+      final id = Random().nextInt(1008).toString();
+      final pokemon = await _restApiService.getPockemon(id);
+      return pokemon;
+    } on Exception catch (e) {
+      return Either.left(e);
+    }
+  }
+}
