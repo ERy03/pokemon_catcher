c2f108cb9245ce653b535a6b70fa9c973dfac154
Ryota <ryoenoki2013@gmail.com>
2023-01-27 11:57:40 +0900

    add: rest api service to get 1 pockemon with id

--- lib/services/rest_api_service.dart
+++ lib/services/rest_api_service.dart
@@ -4,9 +4,10 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:pokemon_catcher/model/pokemon.dart';
+import 'package:pokemon_catcher/model/sprites.dart';
 
 abstract class RestApiServiceAbstract {
-  Future getPockemon(int id);
+  Future getPockemon(String id);
 }
 
 class RestApiService implements RestApiServiceAbstract {
@@ -15,14 +16,35 @@ class RestApiService implements RestApiServiceAbstract {
   final Dio _dio;
 
   @override
-  Future<Either<Exception, Pokemon>> getPockemon(int id) async {
+  Future<Either<Exception, Pokemon>> getPockemon(String id) async {
     try {
       final response = await _dio.get(
         'https://pokeapi.co/api/v2/pokemon/$id',
       );
       if (response.statusCode == 200) {
         return Either.right(
-            Pokemon.fromMap(response.data as Map<String, dynamic>));
+          Pokemon(
+            id: response.data['id'],
+            name: response.data['name'],
+            order: response.data['order'],
+            height: response.data['height'],
+            weight: response.data['weight'],
+            sprites: Sprites(
+              frontDefault: response.data['sprites']['front_default'],
+              frontShiny: response.data['sprites']['front_shiny'],
+              backDefault: response.data['sprites']['back_default'],
+              backShiny: response.data['sprites']['back_shiny'],
+              other: Other(
+                officialArtwork: OfficialArtwork(
+                  frontDefault: response.data['sprites']['other']
+                      ['official-artwork']['front_default'],
+                  frontShiny: response.data['sprites']['other']
+                      ['official-artwork']['front_shiny'],
+                ),
+              ),
+            ),
+          ),
+        );
       } else {
         return Either.left(Exception(response.statusMessage.toString()));
       }
