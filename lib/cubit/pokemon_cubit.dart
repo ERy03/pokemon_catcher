ce3dddabf7021f516f3d73e184d7872b34317801
Ryota <ryoenoki2013@gmail.com>
2023-01-27 12:12:14 +0900

    add: pokemon cubit with getPokemon

--- lib/cubit/pokemon_cubit.dart
+++ lib/cubit/pokemon_cubit.dart
@@ -1,8 +1,24 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:pokemon_catcher/model/pokemon.dart';
+import 'package:pokemon_catcher/repositories/pokemon_repository.dart';
 
 part 'pokemon_state.dart';
 
 class PokemonCubit extends Cubit<PokemonState> {
-  PokemonCubit() : super(PokemonInitial());
+  final PokemonRepository _pokemonRepository;
+  PokemonCubit(this._pokemonRepository) : super(const PokemonInitial());
+
+  void getPokemon() async {
+    emit(const PokemonLoading());
+    _fetchPokemon();
+  }
+
+  _fetchPokemon() async {
+    final result = await _pokemonRepository.getPokemon();
+    result.fold(
+      (l) => emit(PokemonError(l.toString())),
+      (r) => emit(PokemonLoaded(r)),
+    );
+  }
 }
